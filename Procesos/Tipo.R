094bc02d95eadfdc448c4dfa71fb55bd13245a9c
smenaaliaga <smenaaliaga@hotmail.com>
2019-10-12 03:59:06 -0300

    Tipo.R se agrega Tipo-Hardware. Se crea grafico.

--- Procesos/Tipo.R
+++ Procesos/Tipo.R
@@ -21,3 +21,47 @@ ggplot(Tipo, aes(x=Tipo, y=N)) +
   ggtitle("Tipo") +
   ylab("# de tickets") +
   xlab("Tipos") 
+
+#####################
+## Tipo - Servicio ##
+#####################
+
+Tipo_Servicio <- Tickets %>%
+  select(Tipo, Servicio) %>%
+  filter(Servicio != "", Servicio != "Software") %>%
+  group_by(Tipo, Servicio) %>%
+  summarise(N=n())
+
+Tipo_Servicio_Total <- 
+  Tipo_Servicio[!grepl("::", Tipo_Servicio$Servicio),]
+
+## Tipo - Hardware
+ggplot(Tipo_Servicio_Total, aes(x=Tipo, y=N, fill=factor(Servicio))) +
+  geom_bar(stat="identity",position="dodge") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Tipo - Servicio") +
+  ylab("# de tickets") +
+  xlab("Tipos") +
+  scale_fill_discrete(name="Servicio",
+                      breaks=c("Hardware", "Redes"),
+                      labels=c("Hardware", "Red"))
+
+# Tipo - Hardware
+Tipo_Hardware <- Tipo_Servicio %>%
+  filter(str_detect(Servicio, "Hardware"), Servicio != "Hardware",
+         Tipo %in% c("Incidente","Requerimiento"))
+
+Tipo_Hardware$Servicio <- 
+  gsub("Hardware::", "", Tipo_Hardware$Servicio)
+
+Tipo_Hardware <- Tipo_Hardware[!grepl("::", Tipo_Hardware$Servicio),]
+
+ggplot(Tipo_Hardware, aes(x=Servicio, y=N, fill=factor(Tipo))) +
+  geom_bar(stat="identity",position="dodge") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Tipo - Hardware") +
+  ylab("# de tickets") +
+  xlab("Hardware") +
+  scale_fill_discrete(name="Servicio",
+                      breaks=c("Incidente", "Requerimiento"),
+                      labels=c("Incidente", "Requerimiento"))
