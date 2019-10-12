5dfc6f23db3b51e3861d553625a3174175d5b8b3
smenaaliaga <smenaaliaga@hotmail.com>
2019-10-12 02:16:31 -0300

    Se agregar TS Ticket Creados.R, analisis diarios y mensual de # tickets

---
+++ Procesos/TS Ticket Creados.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(ggfortify)
+library(lubridate)
+library(forecast)
+
+setwd("C:/Users/smena/Documents/Proyecto-Ingenieria/Origen de datos")
+
+Tickets <- read.csv2(
+  "TICKET_CREADOS_POR_FECHA_Created_2019-10-11_21-27.csv", 
+  encoding = "UTF-8")
+
+Tickets$Creado <- format(as.Date(Tickets$Creado), format='%Y-%m-%d')
+
+Creado <- Tickets %>%
+  select(Creado) %>%
+  group_by(Creado) %>%
+  summarise(N=n())
+
+ts_Creado_Diarios <- ts(Creado$N, start=c(2015,7), frequency=365)
+
+autoplot(ts_Creado_Diarios) +
+  ggtitle("Tickets creados por día") +
+  ylab("# de tickets") +
+  xlab("días") 
+
+Creado_Mensual <- Creado %>%
+  mutate(MES = month(Creado), ANNO = year(Creado)) %>%
+  group_by(ANNO, MES) %>%
+  select(ANNO, MES, N) %>%
+  summarise(N = sum(N))
+
+Creado_Mensual <- Creado_Mensual[-1,]
+
+ts_Creado_Mensual <- ts(Creado_Mensual$N, start=c(2018,1), frequency=12)
+ts_Creado_Mensual
+
+autoplot(ts_Creado_Mensual) +
+  ggtitle("Tickets creados por mes") +
+  ylab("# de tickets") +
+  xlab("Mes") 
