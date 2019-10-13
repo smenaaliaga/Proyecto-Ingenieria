7d64c43c027b064d8bf8231102b0bab11c04b241
smenaaliaga <smenaaliaga@hotmail.com>
2019-10-13 02:01:49 -0300

    Prioridad.R se obtiene cantidad de tickets y demora de resolución por prioridad y servicio

--- Procesos/Prioridad.R
+++ Procesos/Prioridad.R
@@ -24,3 +24,44 @@ ggplot(Prioridad, aes(x=Prioridad, y=N)) +
   ylab("# de tickets") +
   xlab("Prioridades") +
   geom_text(aes(x = Prioridad, y = N, label = N), vjust = -0.5)
+
+##########################
+## Prioridad - Servicio ##
+##########################
+
+Prioridad_Servicio <- Tickets %>%
+  filter(Servicio != "", Servicio != "Software") %>%
+  group_by(Prioridad, Servicio) %>%
+  summarise(N=n())
+
+Prioridad_Servicio <- Prioridad_Servicio[!grepl("::", Prioridad_Servicio$Servicio),]
+
+ggplot(Prioridad_Servicio, aes(x=Prioridad, y=N, fill = Servicio)) +
+  geom_bar(stat="identity") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  ggtitle("Cantidad de ticket por prioridad y servicio") +
+  ylab("# de tickets") +
+  xlab("Prioridades") +
+  scale_fill_discrete(name="Servicio",
+                      breaks=c("Hardware", "Redes"),
+                      labels=c("Hardware", "Red"))
+
+###########################################
+## Dias resolucion - Prioridad- Servicio ##
+###########################################
+
+resol_prioridad_servicio <- Tickets %>%
+  filter(Tickets$Fecha.de.cierre != "", Servicio != "", Servicio != "Software") %>%
+  mutate(Dia_Resol = as.Date(Fecha.de.cierre) - as.Date(Creado)) %>%
+  select(Prioridad, Servicio, Dia_Resol) 
+
+resol_prioridad_servicio <- 
+  resol_prioridad_servicio[!grepl("::", resol_prioridad_servicio$Servicio),]
+
+ggplot(resol_prioridad_servicio, 
+       aes(x=Prioridad, y=Dia_Resol, fill=Servicio)) + 
+  geom_boxplot(outlier.shape = NA) +
+  coord_cartesian(ylim = c(0,100)) +
+  ggtitle("Demora de resolución por prioridad y servicio") +
+  ylab("Días") +
+  xlab("Prioridades") 
